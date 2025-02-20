92b0ceeecf1be53c971c2af556886bddb50704d9
ritikkashyap720 <72151729+ritikkashyap720@users.noreply.github.com>
2025-02-20 22:59:55 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Services from '../components/services'
+import Services from '../components/Services'
 import Projects from '../components/ProjectsHome'
 import Contact from '../components/Contact'
 import Fotter from '../components/Fotter'
